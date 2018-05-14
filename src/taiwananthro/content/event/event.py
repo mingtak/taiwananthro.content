c50569b2481e393d0b0007273bc692f1c670dc14
Henry Zhuang <henry@mingtak.com.tw>
2018-05-14 12:45:21 +0800

    modified event handles

--- src/taiwananthro/content/event/event.py
+++ src/taiwananthro/content/event/event.py
@@ -4,6 +4,24 @@ from taiwananthro.content import _
 from zope.globalrequest import getRequest
 
 
+def toFolderContents(obj, event):
+    """
+    Return to Folder Contents
+    """
+    request = getRequest()
+    folder = obj.getParentNode()
+    if folder == None:
+        try:
+            folder = api.portal.get()
+        except:
+            return
+    elif getattr(obj, 'portal_type', None) == 'Plone Site':
+        folder = obj
+
+    if request:
+        request.response.redirect('%s/folder_contents' % folder.absolute_url())
+
+
 def back_to_cover(event):
     request = getRequest()
     abs_url = api.portal.get().absolute_url()
@@ -15,5 +33,5 @@ def moveObjectsToTop(obj, event):
     Moves Items to the top of its folder
     """
     folder = obj.getParentNode()
-    if folder != None:
+    if folder != None and hasattr(folder, 'moveObjectsToTop'):
         folder.moveObjectsToTop(obj.id)
